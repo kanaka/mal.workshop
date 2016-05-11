c97e227c690ef3d6b49ad3f1277cda418c478b2e
Joel Martin <github@martintribe.org>
2016-05-11 12:31:04 -0500

    Tutorial: JavaScript Step 5

---
+++ python/step5_tco.py
@@ -0,0 +1,86 @@
+import sys, traceback
+import readline
+import mal_types as types
+import reader, printer
+from env import Env
+import core
+
+# read
+def READ(str):
+    return reader.read_str(str)
+
+# eval
+def eval_ast(ast, env):
+    if type(ast) == types.Symbol:
+        return env.get(ast)
+    elif type(ast) == list:
+        return list(map(lambda a: EVAL(a, env), ast))
+    else:
+        return ast
+
+def EVAL(ast, env):
+  while True:
+    if not type(ast) == list:
+        return eval_ast(ast, env)
+
+    # apply list
+    if len(ast) == 0: return ast
+    a0 = ast[0]
+
+    if "def!" == a0:
+        return env.set(ast[1], EVAL(ast[2], env))
+    elif "let*" == a0:
+        let_env = Env(env)
+        for i in range(0, len(ast[1]), 2):
+            let_env.set(ast[1][i], EVAL(ast[1][i+1], let_env))
+        env = let_env
+        ast = ast[2] # TCO
+    elif "do" == a0:
+        eval_ast(ast[1:-1], env)
+        ast = ast[-1] # TCO
+    elif "if" == a0:
+        a1, a2 = ast[1], ast[2]
+        cond = EVAL(a1, env)
+        if cond is None or cond is False:
+            if len(ast) > 3: ast = ast[3] # TCO
+            else:            return None
+        else:
+            ast = a2 # TCO
+    elif "fn*" == a0:
+        return types.Function(ast[2], ast[1], env)
+    else:
+        el = eval_ast(ast, env)
+        f = el[0]
+        if type(f) == types.Function:
+            ast = f.ast
+            env = Env(f.env, f.params, el[1:]) # TCO
+        else:
+            return f(*el[1:])
+
+
+# print
+def PRINT(exp):
+    return printer.pr_str(exp)
+
+# repl
+repl_env = Env()
+def REP(str):
+    return PRINT(EVAL(READ(str), repl_env))
+
+# core.py: defined using python
+for k,v in core.ns.items(): repl_env.set(types.Symbol(k), v)
+
+# core.mal: defined using the language itself
+REP("(def! not (fn* (a) (if a false true)))")
+
+# repl loop
+while True:
+    try:
+        line = input("user> ")
+        if line == None: break
+        if line == "": continue
+        print(REP(line))
+    except reader.Blank: continue
+    except EOFError: break
+    except Exception as e:
+        print("".join(traceback.format_exception(*sys.exc_info())))
---
+++ ruby/step5_tco.rb
@@ -0,0 +1,100 @@
+require "readline"
+require_relative "types"
+require_relative "reader"
+require_relative "printer"
+require_relative "env"
+require_relative "core"
+
+# read
+def READ(str)
+    return read_str(str)
+end
+
+# eval
+def eval_ast(ast, env)
+    return case ast
+        when Symbol
+            env.get(ast)
+        when Array
+            Array.new ast.map{|a| EVAL(a, env)}
+        else
+            ast
+    end
+end
+
+def EVAL(ast, env)
+  while true
+    if not ast.is_a? Array
+        return eval_ast(ast, env)
+    end
+    if ast.empty?
+        return ast
+    end
+
+    # apply list
+    a0,a1,a2,a3 = ast
+    case a0
+    when :def!
+        return env.set(a1, EVAL(a2, env))
+    when :'let*'
+        let_env = Env.new(env)
+        a1.each_slice(2) do |a,e|
+            let_env.set(a, EVAL(e, let_env))
+        end
+        env = let_env
+        ast = a2 # TCO
+    when :do
+        eval_ast(ast[1..-2], env)
+        ast = ast.last # TCO
+    when :if
+        cond = EVAL(a1, env)
+        if not cond
+            return nil if a3 == nil
+            ast = a3 # TCO
+        else
+            ast = a2 # TCO
+        end
+    when :"fn*"
+        return Function.new(a2, a1, env) {|*args|
+            EVAL(a2, Env.new(env, a1, Array.new(args)))
+        }
+    else
+        el = eval_ast(ast, env)
+        f = el[0]
+        if f.class == Function
+            ast = f.ast
+            env = Env.new(f.env, f.params, el.drop(1)) # TCO
+        else
+            return f[*el.drop(1)]
+        end
+    end
+  end
+end
+
+# print
+def PRINT(exp)
+    return pr_str(exp)
+end
+
+# repl
+repl_env = Env.new(nil)
+
+def REP(str, env)
+    return PRINT(EVAL(READ(str), env))
+end
+
+# core.rb: defined using ruby
+$core_ns.each do |k,v| repl_env.set(k,v) end
+
+# core.mal: defined using mal itself
+REP("(def! not (fn* (a) (if a false true)))", repl_env)
+
+# repl loop
+while line = Readline.readline("user> ", true)
+    begin
+        puts REP(line, repl_env)
+    rescue Exception => e
+        puts "Error: #{e}"
+        puts "\t#{e.backtrace.join("\n\t")}"
+    end
+end
